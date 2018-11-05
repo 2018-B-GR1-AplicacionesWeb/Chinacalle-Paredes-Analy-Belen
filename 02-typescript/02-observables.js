0281b66d37ab4fbf25bf7c7dcaae23f0f997544d
Analy <analy_chinacalle@hotmail.com>
2018-11-05 10:58:53 -0500

    observables y asyncrono

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -2,13 +2,30 @@
 //import * as rxjs from 'rxjs';
 //import {Observable} from "rxjs";
 const rxjs = require('rxjs');
-const observableUno$ = rxjs.of(1, 2, 3, 4, 5, 6, 7); //devuelve un observable
+const map = require('rxjs/operators').map;
+const observableUno$ = rxjs.of([1, 2, 3], 'Hola', 3, true, 5, new Date(), 7); //devuelve un observable
 console.log(observableUno$);
 observableUno$
-    .subscribe((ok) => {
-    console.log(ok);
-}, (error) => {
-    console.log(error);
-}, () => {
-    console.log('Completado');
-});
+    .pipe(map((valor) => {
+    console.log('valor: ', valor);
+    return {
+        data: valor
+    };
+}));
+const promesita = () => {
+    return new Promise((resolve, reject) => {
+        reject(':C');
+    });
+};
+async function ejecutarCodigoSyncrono() {
+    console.log('Inicio');
+    try {
+        const resultadoPromesita = await promesita();
+        console.log(resultadoPromesita);
+    }
+    catch (e) {
+        console.log('Error ', e);
+    }
+    console.log('Fin');
+}
+ejecutarCodigoSyncrono();
--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -4,11 +4,22 @@
 
 declare var require: any;
 const rxjs =require('rxjs');
-const observableUno$ = rxjs.of(1,2,3,4,5,6,7);   //devuelve un observable
+const map = require('rxjs/operators').map;
+const observableUno$ = rxjs.of([1,2,3],'Hola',3,true,5,new Date(),7);   //devuelve un observable
 console.log(observableUno$);
 
 observableUno$
-    .subscribe(
+    .pipe(
+        map(
+            (valor) => {
+                console.log('valor: ',valor);
+                return {
+                    data: valor
+                };
+            }
+        )
+    )
+/*    .subscribe(
         (ok) => {
             console.log(ok);
         },
@@ -18,5 +29,26 @@ observableUno$
         () => {
             console.log('Completado');
         }
-    );
+    )*/;
 
+const promesita = ()=> {
+    // @ts-ignore
+    return new Promise(
+        (resolve, reject) => {
+            reject(':C');
+        }
+    )
+};
+
+async function ejecutarCodigoSyncrono() {
+    console.log('Inicio');
+    try {
+        const resultadoPromesita = await promesita();
+        console.log(resultadoPromesita);
+    }catch (e) {
+        console.log('Error ', e);
+    }
+    console.log('Fin');
+}
+
+ejecutarCodigoSyncrono();
