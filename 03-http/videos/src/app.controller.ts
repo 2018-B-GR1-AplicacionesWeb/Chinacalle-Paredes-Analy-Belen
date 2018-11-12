096901d3a60d29fe7ca419c2144f942678ef323a
Analy <analy_chinacalle@hotmail.com>
2018-11-12 11:00:32 -0500

    controlador http

--- 03-http/videos/src/app.controller.ts
+++ 03-http/videos/src/app.controller.ts
@@ -1,12 +1,77 @@
-import { Get, Controller } from '@nestjs/common';
+import {Get, Controller, HttpCode, InternalServerErrorException, Post, Query, Param} from '@nestjs/common';
 import { AppService } from './app.service';
-
-@Controller()
+import {async} from "rxjs/internal/scheduler/async";
+import {Observable, of} from "rxjs";
+//COntrolador gestiona Request y Response
+@Controller()   //decorador   --> @nombre(paréntesis)   Es una función
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  root(): string {
-    return this.appService.root();
+    //http://ip:puerto
+  @Get()    //Con esto se indica que el método se ejecuta cuando se haga un request con metodo get
+  @HttpCode(200) //status
+  raiz(
+      @Query() todosQueryParams: any,
+      @Query('nombre') nombre: string,
+  ): string {
+      console.log(todosQueryParams);
+    return 'Hola Metrocida'+nombre;
   }
+  @Get('segmentoUno/segmentoDos/:idUsuario')
+  parametroRuta(
+      @Param('idUsuario') id
+  ){
+      return id;
+
+  }
+
+  @Get('adiosMundo')
+  adiosMundo(): string {
+      return 'Adios Mundo'
+  }
+
+  @Post('adiosMundo')
+  adiosMundoPost(): string {
+      return 'Adios mundo POST'
+  }
+
+  @Get('adiosMundoPromesa')
+    adiosMundoPromesa(): Promise<string> {
+        const promesaAdios = (): Promise<string> => {
+            return new Promise(
+                (resolve ) => {
+                    resolve('AdiosMundo');
+                }
+            )
+        };
+        return promesaAdios();
+    }
+
+    @Get('adiosMundoAsync')
+    @HttpCode(201)      //Si sale bien se envía este codigoooooooooooo
+    async adiosMundoAsync(): Promise<string> {
+        const promesaAdios = (): Promise<string> => {
+            return new Promise(
+                (resolve, reject ) => {
+                    reject('AdiosMundo');
+                }
+            )
+        };
+        try{
+            const respuesta: string = await promesaAdios();
+            return promesaAdios();
+        }catch (e) {
+            console.error(e);
+            throw new InternalServerErrorException({mensaje:'Error servidor'})
+
+        }
+
+    }
+
+    @Get('adiosMundoObservable')
+    adiosMundoObservable(): Observable<string>{
+      const respuesta$ = of('adios Mundo');
+      return respuesta$;
+    }
+
 }
--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {AppModule } from './app.module';    //. es para entrar al mismo directorio
+//const http_server = require('');  importar javascript
+import *as httpserver from 'http-server';
+
+console.log(httpserver);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000);   //puerto en el que se levanta el servidor
 }
 bootstrap();
